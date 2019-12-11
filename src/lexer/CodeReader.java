d3216612f648ac8a401f96d02ada57fd0c4ad524
Miranda Masters <mastermk@rose-hulman.edu>
2019-12-11 15:41:07 -0500

    code is read in

--- src/lexer/CodeReader.java
+++ src/lexer/CodeReader.java
@@ -1,8 +1,60 @@
 package lexer;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class CodeReader {
 
-	public String readFile(String filename){
-		return null;
+	public String readFile(String filename) {
+		return stripComments(readInFile(filename));
+	}
+
+	private String stripComments(String s) {
+		char[] letters = s.toCharArray();
+		int len = s.length();
+		StringBuilder builder = new StringBuilder(len);
+		boolean inBlockComment = false;
+		boolean inLineComment = false;
+		for (int i = 0; i < len; i++) {
+			if (inBlockComment) {
+				if (letters[i] == '*' && i < len - 1 && letters[i + 1] == '/') {
+					inBlockComment = false;
+					i++;
+				}
+			} else if (inLineComment) {
+				if (letters[i] == '\n') {
+					inLineComment = false;
+				}
+			} else {
+				if (letters[i] == '/' && i < len - 1) {
+					if (letters[i + 1] == '*') {
+						inBlockComment = true;
+						i++;
+					}
+					if (letters[i + 1] == '/') {
+						inLineComment = true;
+						i++;
+					}
+				} else {
+					builder.append(letters[i]);
+				}
+			}
+
+		}
+		return builder.toString();
+	}
+
+	private String readInFile(String filename) {
+		StringBuilder builder = new StringBuilder();
+		try (Scanner scanner = new Scanner(new File(filename))) {
+			while (scanner.hasNext()) {
+				builder.append(scanner.nextLine() + "\n");
+			}
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+			System.exit(0);
+		}
+		return builder.toString();
 	}
 }
--- src/main/Main.java
+++ src/main/Main.java
@@ -1,10 +1,12 @@
 package main;
 
+import lexer.CodeReader;
 import lexer.Tagger;
 
 public class Main {
 
 	public static void main(String[] args) {
+		System.out.println(new CodeReader().readFile("sampleCode/shortExample.java"));
 		System.out.println(new Tagger().tag("aaa = 6;\nSystem.out.println(aaa+5);").toString());
 	}
 
