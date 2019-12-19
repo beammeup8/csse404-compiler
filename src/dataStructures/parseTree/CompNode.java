7a4b601f7bcadba86ea9025b83527bfbc827f85b
Miranda Masters <mastermk@rose-hulman.edu>
2019-12-18 20:52:22 -0500

    first half of nodeFactory done

---
+++ src/dataStructures/parseTree/CompNode.java
@@ -0,0 +1,26 @@
+package dataStructures.parseTree;
+
+import java.util.List;
+
+import dataStructures.ParserType;
+import dataStructures.Tag;
+
+public class CompNode extends Node {
+
+	public CompNode(List<Tag> tags, IntWrap head) {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public ParserType getType() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void optimize() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- src/dataStructures/parseTree/ProgramNode.java
+++ src/dataStructures/parseTree/ProgramNode.java
@@ -5,9 +5,14 @@ import java.util.List;
 import dataStructures.ParserType;
 import dataStructures.Tag;
 
+/**
+ * Program -> MainClassDecl ClassDecl
+ * 
+ * @author mastermk beemanla
+ *
+ */
 public class ProgramNode extends Node {
 	
-	// Program -> MainClassDecl ClassDecl
 	public ProgramNode(List<Tag> tags, IntWrap head) throws Exception {
 		int initialHead = head.integer;
 		addNonTerminal(tags, head, initialHead, ParserType.MainClassDecl);
